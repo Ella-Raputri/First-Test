91294e962f43c60572a2a1e07bfba4909feefd0e
Ella Raputri <ellaraputri@gmail.com>
2023-09-16 17:13:01 +0700

    Test

---
+++ test1.py
@@ -0,0 +1,24 @@
+#Coding testing using python
+age = 18
+print ("Happy " + str(age) +"th birthday!\n")
+
+print(2+6)
+print(2**3)
+print(10-2)
+print(4*2)
+print(8/1)
+print(8//1)
+print(40%16)
+
+#// is for integer division % for modulo
+
+a = 8//1
+b = 8/1
+
+print(type(a))
+print(type(b))
+
+fav_num = 88
+print ("my fav number is "+ str(fav_num) + "!\n")
+
+#import this #importing Python zen
---
+++ test2.py
@@ -0,0 +1,4 @@
+fruits = ['mango', 'strawberry', 'apple', 'orange']
+
+for i in range(0,4):
+    print(fruits[i])
